f6fd5e743ba27ccce6ea513285104c8e89d56008
mansi.modi <mansi.modi@streebo.com>
2023-08-09 18:17:44 +0530

    - npm i react and react-dom - import react and react-dom - remove cdn links script - npm i parcel - added browserlist

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const h1 = React.createElement("h1", {
-    "id": "heading1"
+    id: "heading1"
 }, "Hello World");
 const h2 = React.createElement("h2", {
-    "id": "heading2"
+    id: "heading2"
 }, "Hello World");
 const container = React.createElement("div", {
     "id": "container"
